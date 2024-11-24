eaebaa3fee5a1cb4c7837458431a5ae800aaaeb4
Rahul Kumar <rahul@napstack.com>
2024-11-24 17:57:38 +0530

    refactor (baseMiddleware): add an error logger + update return its return type

--- app/devhunt-api/src/core/base/abstact.class.base.middleware.ts
+++ app/devhunt-api/src/core/base/abstact.class.base.middleware.ts
@@ -1,16 +1,21 @@
 import { Response } from "express";
 
 import Base from "./abstract.class.base";
+import Logging from "src/library/logging";
 
 export default abstract class BaseMiddlewareService extends Base {
 
-    protected async adapter(service: void | PromiseLike<void>, res: Response): Promise<void | any> {
+    protected async adapter(service: void | PromiseLike<void> | Promise<void | Response>, res: Response): Promise<void | any> {
 
         try {
             await service;
             
-        } catch (err) {
-
+        } catch (err: any) {
+            
+            Logging.error(err.message);
         }
     }
+
+    /** Helper methods **/
+
 }
