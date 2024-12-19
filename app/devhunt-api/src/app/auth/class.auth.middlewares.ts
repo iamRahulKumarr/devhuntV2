02996a3351b33b80d5385bf6c7eda56b483a9d4b
Rahul Kumar <rahul@napstack.com>
2024-12-19 14:30:20 +0530

    feat (auth): create restrictTo middleware

--- app/devhunt-api/src/app/auth/class.auth.middlewares.ts
+++ app/devhunt-api/src/app/auth/class.auth.middlewares.ts
@@ -5,6 +5,8 @@ import { env } from "../../envConfig";
 import User from "../users/user.model";
 import BaseMiddlewareService from "../../core/base/abstact.class.base.middleware";
 import AppError from "../handlers/error-handler/class.AppError";
+import catchAsync from "../handlers/error-handler/catchAsync";
+import { UserType } from "src/types/users/user";
 
 export default class AuthMiddlewareService extends BaseMiddlewareService {
 
@@ -17,10 +19,9 @@ export default class AuthMiddlewareService extends BaseMiddlewareService {
 
     public protect() {
 
-        return async (req: Request, _: any, next: NextFunction): Promise<void> => {
+        return catchAsync(async (req: Request, _: any, next: NextFunction) => {
 
-            let accessToken: string | null = req.cookies.accessToken;
-
+            let accessToken: string | null = req.cookies['Access-Token'];
 
             if (!accessToken) {
                 return next(new AppError('Not Authorized', 401))
@@ -45,7 +46,19 @@ export default class AuthMiddlewareService extends BaseMiddlewareService {
 
             next();
 
-        }
+        })
 
+    }
+
+    public restrictTo(...userRole: UserType[]) {
+
+        return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+            
+            if(!userRole.includes((req as any).user.userType)){
+                return next(new AppError('Not Authorized', 401))
+            }
+
+            next();
+        })
     }
 }
