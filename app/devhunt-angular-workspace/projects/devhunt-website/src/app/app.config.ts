a5a5dfdc4c2d996b770a2990e822b33300ae4d9a
Rahul Kumar <rahul@napstack.com>
2025-01-12 18:23:43 +0530

    refactor (devhunt-website): update app component and app config

--- app/devhunt-angular-workspace/projects/devhunt-website/src/app/app.config.ts
+++ app/devhunt-angular-workspace/projects/devhunt-website/src/app/app.config.ts
@@ -1,6 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+  ]
 };
